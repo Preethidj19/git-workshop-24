02f0d83a45fb67401e27332506dcb0a9cd7adbd7
Preethi DJ <nnm23ad038@nmamit.in>
2024-09-28 12:34:55 +0530

    Preethi

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "preethidj19",
+    name: "Preethi D J",
+    quote: "I am a Student",
+  },
 ];
